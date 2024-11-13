556a8f69510bad0bdcec7cd19a6680efdf18fd39
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-13 19:51:32 +0530

    Update bot

---
+++ bot/config.py
@@ -0,0 +1,56 @@
+from os import environ as env
+
+class Telegram:
+    API_ID = int(env.get("API_ID", ""))
+    API_HASH = env.get("API_HASH", "")
+    BOT_TOKEN = env.get("BOT_TOKEN", "")
+    BOT_USERNAME = env.get("BOT_USERNAME", "ReactGenieBot")
+    EMOJIS = [
+        "👍", "👎", "❤", "🔥", 
+        "🥰", "👏", "😁", "🤔",
+        "🤯", "😱", "🤬", "😢",
+        "🥶", "🤩", "🤮", "💩",
+        "🙏", "👌", "🤣", "🤡",
+        "🥱", "🥴", "😍", "🤓",
+        "❤‍🔥", "🌚", "😐", "💯",
+        "🤣", "⚡", "🍌", "🏆",
+        "💔", "🤨", "😐", "😡",
+        "👅", "🆒", "🖕", "😈",
+        "😴", "😭", "👻", "⚡",
+        "👨‍💻", "👀", "🎃", "🙄",
+        "😇", "😨", "🤝", "🤐",
+        "🤗", "🫡", "🎅", "🥸",
+        "🤫", "😶‍🌫", "🤪", "😏",
+        "😘", "👾", "🤷‍♂", "😎"
+    ]
+
+LOGGER_CONFIG_JSON = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s][%(name)s][%(levelname)s] -> %(message)s',
+            'datefmt': '%d/%m/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'file_handler': {
+            'class': 'logging.FileHandler',
+            'filename': 'event-log.txt',
+            'formatter': 'default'
+        },
+        'stream_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        }
+    },
+    'loggers': {
+        'bot': {
+            'level': 'INFO',
+            'handlers': ['file_handler', 'stream_handler']
+        },
+        'pyrogram': {
+            'level': 'INFO',
+            'handlers': ['file_handler', 'stream_handler']
+        }
+    }
+}
