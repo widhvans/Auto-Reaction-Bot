253f16c577d38559e94f9fcdb760d0811baadac5
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-13 19:49:26 +0530

    Update

---
+++ bot/plugins/commands.py
@@ -0,0 +1,53 @@
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bot import TelegramBot
+from bot.config import Telegram
+from bot.static import *
+
+@TelegramBot.on_message(
+    filters.command('start')
+    & (
+        filters.private |
+        filters.group
+    )
+)
+async def start_command(_, msg: Message):
+    return await msg.reply(
+        text=WelcomeText % {'first_name': msg.from_user.first_name if msg.from_user else 'Anonymous'},
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text='⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆', url=f'https://telegram.me/{Telegram.BOT_USERNAME}?startgroup=botstart')
+                ],
+                [
+                    InlineKeyboardButton(text='• ᴜᴩᴅᴀᴛᴇꜱ •', url='https://telegram.me/RahulReviewsYT'),
+                    InlineKeyboardButton(text='• ꜱᴜᴩᴩᴏʀᴛ •', url='https://telegram.me/CodeXSupport')
+                ],
+                [
+                    InlineKeyboardButton(text='⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ⇆', url=f'https://telegram.me/{Telegram.BOT_USERNAME}?startchannel=botstart')
+                ]
+            ]
+        )
+    )
+
+@TelegramBot.on_message(
+    filters.command('help')
+    & (
+        filters.private |
+        filters.group
+    )
+)
+async def send_emojis(_, msg: Message):
+    return await msg.reply(
+        text=Help,
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text='👨‍💻 ᴏᴡɴᴇʀ', url='https://telegram.me/CodeXBro'),
+                    InlineKeyboardButton(text='💥 ʀᴇᴘᴏ', url='https://github.com/CodeXBots/Auto-Reaction-Bot')
+                ]
+            ]
+        )
+    )
