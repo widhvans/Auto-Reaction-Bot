9651a9ff2cc5ab9b0a8f6f219a9dbc1e31d35f21
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-07 19:25:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "")
-API_ID = int(environ.get("API_ID", ""))
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-DATABASE_URL = environ.get("DATABASE_URL", "")
+API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
+API_ID = int(environ.get("API_ID", "27461953"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7191299411:AAEwGLIbGF0ATvpZ1F06MrYRfNiYX1xcwIA")
+BOT_OWNER = int(environ.get("BOT_OWNER", "-1001814841940"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "Gsiege_sksgwvv_bot")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001684575211"))
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Define default emojis list
 EMOJIS = [
