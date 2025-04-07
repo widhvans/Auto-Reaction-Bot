102051aaa9ae49f02a909323b3e0a35407d9a992
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-08 02:58:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 
 API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
 API_ID = int(environ.get("API_ID", "27461953"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7191299411:AAEwGLIbGF0ATvpZ1F06MrYRfNiYX1xcwIA")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7530428356:AAEIqnXFYd4yuKTcS12LpmziyPM9phTpQDc")
 BOT_OWNER = int(environ.get("BOT_OWNER", "-1001938030055"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Gsiege_sksgwvv_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
