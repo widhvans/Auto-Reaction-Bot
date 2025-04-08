83be7bfadb6b3031ebcfbff5546e7bf238f18cf4
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-08 16:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 from os import environ
 
 API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
@@ -9,12 +8,3 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "Auto_Post_React_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001684575211"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-# Define default emojis list
-EMOJIS = [
-    "👍", "🤷‍♂", "❤", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", 
-    "🥶", "🤩", "🥳", "😎", "🙏", "👌", "🤣", "😇", "🥱", "🥴", "😍", "🤓", 
-    "❤‍🔥", "🌚", "😐", "💯", "🦄", "⚡", "👾", "🏆", "💔", "🤨", "🌟", "😡", 
-    "👅", "🆒", "😘", "😈", "😴", "😭", "👻", "🌈", "👨‍💻", "👀", "🎃", "🙄", 
-    "🤧", "😨", "🤝", "🤐", "🤗", "🫡", "🤭", "🥸", "🤫", "😶‍🌫", "🤪", "😏"
-]
