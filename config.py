78c1109f0a525f4f06dc684d0ae92d66e3bccc0e
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-14 11:44:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,28 @@
-
 from os import environ
 
 API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
 API_ID = int(environ.get("API_ID", "27461953"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7978391610:AAH8QfTjlDJ9aat58WAAF0a7HwblsxhT7EI")
-# Updated to your actual user ID
-BOT_OWNER = environ.get("BOT_OWNER", "1938030055")  # Changed to your real ID
+BOT_OWNER = environ.get("BOT_OWNER", "1938030055")
 BOT_USERNAME = environ.get("BOT_USERNAME", "Auto_Post_React_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001684575211"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# List of 15 army bot tokens
+ARMY_BOT_TOKENS = [
+    "7867128410:AAEJKkCCIWiXeksYvSJruQ00908rMm7sMz4",
+    "7432126339:AAF4AQaN_2LEreEaF1pX7vd6MxzRv8yCzmQ",
+    "7966299063:AAH5R8jFRjSvLgIDUPmZMosHTRVKRLR3TyM",
+    "7561783547:AAFj3vYPKuRtZEzABLQ5nGvFXLDNC9S7WHQ",
+    "8001535737:AAGH1wSwuTv_mVxFtYJG1nmr1nSnlBxtV54",
+    "7900234155:AAEu5O4Ax9hEZiynsEXSS6Or8bJxUo4wguU",
+    "7982893821:AAE-anM1L5BrJ7_oWr3pHZ7OFnKNfZnFUtI",
+    "8009774147:AAFhQrRZIjh9oGsl-RxAXDxUUWilC9yQcX8",
+    "7977931604:AAEgQgV9jsoVi_LqTsXQBYgiaAzhGu8NSps",
+    "7610444558:AAHS3cv_varPgrdx9_MIqrBOMYmqSD3KwpA",
+    "8144659042:AAHgEP30K-eR4E0iWCwheOXoVtJVrY8yFoo",
+    "7656387892:AAEhhS_2KI9wR_YCttXjv4zi4AJUZIrfRMM",
+    "7650729995:AAHWRBYyKM54vIZpD5b2J0Ps_3OHAjiUrwk",
+    "7560769905:AAFsfIpjTgao8plyeAdLel-hv3AkkTsNGjQ",
+    "8095770878:AAG0q5yZhFjTJX_DfHJP-_38lMQFdXEyReQ",
+]
