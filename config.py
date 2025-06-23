65bda77d52a53f55d0978e0d4d84aab64667f26c
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-23 17:25:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 
 API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
 API_ID = int(environ.get("API_ID", "27461953"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7978391610:AAH8QfTjlDJ9aat58WAAF0a7HwblsxhT7EI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7978391610:AAE_H6GUjs3kFpwkwitKLnLq2s34EW7fzho")
 BOT_OWNER = int(environ.get("BOT_OWNER", "1938030055")) # Make sure this is an integer
 BOT_USERNAME = environ.get("BOT_USERNAME", "Auto_Post_React_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
