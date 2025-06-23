47a3b159a81f60440088fc2c750565ee9f79a508
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-23 17:57:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,10 @@ from os import environ
 API_HASH = environ.get("API_HASH", "8a19a6a007044ff7b41ada4b377cdfba")
 API_ID = int(environ.get("API_ID", "27461953"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "7978391610:AAE_H6GUjs3kFpwkwitKLnLq2s34EW7fzho")
-BOT_OWNER = int(environ.get("BOT_OWNER", "1938030055")) # Make sure this is an integer
+BOT_OWNER = int(environ.get("BOT_OWNER", "1938030055"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Auto_Post_React_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001524622686"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001684575211"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-UPDATE_CHANNEL = "https://t.me/joinnowearn"
 
-# Positive Telegram reaction emojis only
-VALID_EMOJIS = ["👍", "❤️", "🔥", "🎉", "👏", "🥰", "🤩", "👌", "💯"]
-
-# The ARMY_BOT_TOKENS list has been removed from here.
-# The army is now managed dynamically via the bot owner commands and stored in the database.
+# The ARMY_BOT_TOKENS list is removed from here. It will be managed via the database.
